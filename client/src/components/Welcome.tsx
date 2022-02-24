0c57644e9a9d37b1992014e8fcf289246e4a2721
Xiang Li <bylixiang@gmail.com>
2022-02-23 21:37:12 -0800

    show dummy transactions

--- client/src/components/Welcome.tsx
+++ client/src/components/Welcome.tsx
@@ -5,7 +5,7 @@ import { BsInfoCircle } from "react-icons/bs";
 import { Loader } from "./";
 import { useContext } from "react";
 import { TransactionContext } from "../context/TransactionContext";
-
+import { shortenAddress } from "../utils/shortenAddress";
 const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
 type InputProps = {
@@ -94,7 +94,7 @@ const Welcome = () => {
                             <div>
                                 <p className="text-white font-light text-sm">
                                     {/* {shortenAddress(currentAccount)} */}
-                                    0x address
+                                    {shortenAddress(connectedAccount)}
                                 </p>
                                 <p className="text-white font-semibold text-lg mt-1">
                                     Ethereum
